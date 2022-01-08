252965952c2d865b0e86b9d423ed369fb3868052
umangkumarumk <umangkumarumk@gmail.com>
2022-01-08 15:12:19 +0530

    day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class homepage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final int days = 30;
+    final String name = "umang";
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("first app"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text(
+              "Hi this is $name Learining Flutter course of $days days!!!!"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/home_page.dart';
 
 void main() {
   runApp(myapp());
 }
 
 class myapp extends StatelessWidget {
-  const myapp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-       child: Center(
-         child: Container(
-        child: Text("Learining Flutter!!!!"),
-      ),
-       ),
-       
-    ),
-    
+      home: homepage(),
     );
   }
 }
