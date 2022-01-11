ff3991ff2e7bf232ed63907abc6fb6c8d3a2baa2
umangkumarumk <umangkumarumk@gmail.com>
2022-01-11 15:11:37 +0530

    day 5 completed

--- lib/pages/main.dart
+++ lib/pages/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/home_page.dart';
 import 'package:flutter_application_1/pages/loginpage.dart';
+import 'package:flutter_application_1/utils/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 void main() {
   runApp(MyApp());
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
      // home: homepage(),
+     //debugShowCheckedModeBanner: false,
      themeMode: ThemeMode.light,
      theme: ThemeData(
        primarySwatch: Colors.pink,
@@ -24,8 +26,8 @@ class MyApp extends StatelessWidget {
      initialRoute: "/login",
       routes: {
         "/" : (context)=> LoginPage(),
-        "/home": (context) => homepage(),
-        "/login" : (context)=> LoginPage(),
+        MyRoutes.homeroute: (context) => homepage(),
+        MyRoutes.loginroute : (context)=> LoginPage(),
       },
     );
   }
